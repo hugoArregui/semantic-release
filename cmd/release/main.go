9f6803195fa83a2508208cc51ffe961d3cfb8214
Hugo Arregui <hugo.arregui@gmail.com>
2019-09-30 21:40:58 -0300

    feat(x): do

--- cmd/release/main.go
+++ cmd/release/main.go
@@ -3,12 +3,11 @@ package main
 import (
 	"github.com/hugoArregui/semantic-release/internal/release"
 	"os"
+	"strings"
 	"log"
 )
 
 func main() {
-	fromCommit := "0aff6e71f82ccc90697a005386f38ddc79d09cbc"
-
 	ghToken, ok := os.LookupEnv("GITHUB_TOKEN")
 	if !ok {
 		log.Fatal("missing GITHUB_TOKEN")
@@ -19,24 +18,27 @@ func main() {
 		log.Fatal("missing TRAVIS_BRANCH")
 	}
 
-	commit, ok := os.LookupEnv("TRAVIS_COMMIT")
-	if ok {
-		log.Println("TRAVIS COMMIT", commit)
+	commitRange, ok := os.LookupEnv("TRAVIS_COMMIT_RANGE")
+	if !ok {
+		log.Fatal("missing TRAVIS_COMMIT_RANGE")
 	}
 
-	commitRange, ok := os.LookupEnv("TRAVIS_COMMIT_RANGE")
-	if ok {
-		log.Println("TRAVIS COMMIT RANGE", commitRange)
+	slug, ok := os.LookupEnv("TRAVIS_REPO_SLUG")
+	if !ok {
+		log.Fatal("missing TRAVIS_REPO_SLUG")
 	}
 
 	// pr, ok := os.LookupEnv("TRAVIS_PULL_REQUEST")
+	commits := strings.Split(commitRange, "...")
+	repo := strings.Split(slug, "/")
 
 	config := release.Config{
-		FromCommit: fromCommit,
+		FromCommit: commits[0],
+		ToCommit: commits[1],
 		GHToken: ghToken,
-		Owner: "hugoArregui",
-		Repo: "semantic-release",
-Branch: branch,
+		Owner: repo[0],
+		Repo: repo[1],
+		Branch: branch,
 	}
 
 	if err := release.SemanticRelease(config); err != nil {
