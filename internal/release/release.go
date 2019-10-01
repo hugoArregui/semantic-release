0a12dc1f848a83bc2962e3a78a2f8a29bf98a6c6
Hugo Arregui <hugo.arregui@gmail.com>
2019-09-30 21:57:27 -0300

    feat(x): do x

--- internal/release/release.go
+++ internal/release/release.go
@@ -6,6 +6,7 @@ import (
 	"regexp"
 	"strings"
 
+	"github.com/Masterminds/semver"
 	"github.com/google/go-github/github"
 	"golang.org/x/oauth2"
 )
@@ -29,17 +30,6 @@ func SemanticRelease(config Config) error {
 	}
 
 	fmt.Println("commits", commits)
-
-	if config.Branch == "master" {
-		ctx := context.TODO()
-		oauthClient := oauth2.NewClient(ctx, oauth2.StaticTokenSource(&oauth2.Token{AccessToken: config.GHToken}))
-		ghClient := github.NewClient(oauthClient)
-		release, err := getLatestRelease(ctx, ghClient, config.Owner, config.Repo, "")
-		if err != nil {
-			return err
-		}
-		fmt.Println("latest release:", release)
-	}
 
 	newReleaseType := "patch"
 
@@ -90,6 +80,31 @@ Please see https://github.com/angular/angular.js/blob/master/DEVELOPERS.md#commi
 	}
 
 	fmt.Println("new release type is", newReleaseType)
+
+	if config.Branch == "master" {
+		ctx := context.TODO()
+		oauthClient := oauth2.NewClient(ctx, oauth2.StaticTokenSource(&oauth2.Token{AccessToken: config.GHToken}))
+		ghClient := github.NewClient(oauthClient)
+		latestRelease, err := getLatestRelease(ctx, ghClient, config.Owner, config.Repo, "")
+		if err != nil {
+			return err
+		}
+
+		latestVersion := latestRelease.Version
+		fmt.Println("latest version:", latestVersion.String())
+
+		var newVersion semver.Version
+		switch newReleaseType {
+		case "major":
+			newVersion = latestVersion.IncMajor()
+		case "minor":
+			newVersion = latestVersion.IncMinor()
+		case "feat":
+			newVersion = latestVersion.IncPatch()
+		}
+
+		fmt.Println("new version:", newVersion.String())
+	}
 
 	return nil
 }
